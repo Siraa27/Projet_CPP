e2c51429a109ff5f45ef08c82394ed7638c0111a
Siraa27 <Siraa27@users.noreply.github.com>
2021-04-06 15:01:17 +0200

    rajout de dyoxygen

--- troisième rendu/Couche.hpp
+++ troisième rendu/Couche.hpp
@@ -4,13 +4,47 @@
  * \file Couche.hpp
  * \brief Les propritées d'une couche : ses nombre de neurones ainsi que sa fonction d'activation
  */
-class Couche{
-    protected : 
-        int nbNeurones; 
-        Neurone Neurones[]; 
-        int indice ; 
-    public :
-        Couche (const nbNeurones); // le nb de Neurones d'une couche ne change pas une fois choisit au cours du programmes
-        double foncActivation();
-}
+/*! \namespace Les_couches_du_réseau
+ * une classe qui permets de créer des couches du réseau
+ */
+namespace Les_couches_du_réseau 
+{
+    /*! \class Couche
+   * \brief classe representant une couche
+   *
+   *  La classe génère les couches du réseau
+   */
+    class Couche{
+
+         protected : 
+          int nbNeurones; /*!< Le nombre de neurones d'une couches*/
+          Neurone Neurones[]; /*!< Le tableau qui contient tous les neurones d'une couche*/
+          int indice ; /*!< L'indice d'un neurone*/
+
+         public :
+             /*!
+             *  \brief Constructeur
+             *
+             *  Constructeur de la classe Couche
+             *
+             *  \param nbNeurones : le nombre de neurones par couche, qui est fixe d'ou le "const"
+             */
+          Couche (const nbNeurones); // le nb de Neurones d'une couche ne change pas une fois choisit au cours du programmes
+             /*!
+             *  \brief Destructeur
+             *
+             *  Destructeur de la classe Couche
+             */
+          virtual ~Couche();
+             /*!
+             *  \brief La fonction d'activation 
+             *
+             *  Methode qui permet d'avoir la sortie d'une couche
+             *
+             *  \return la valeur en sortie d'une couche 
+             *  pour l'exemple des iris de Fisher elle doit nous fournir un vecteur (1,0,0) si la fleur appartient à la classe 1
+             */
+          double foncActivation();
+    };
+};
 #endif
