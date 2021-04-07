ba246e6d7925b174811002db117800369e07d70c
camillegoujet <camille.goujet@insa-rouen.fr>
2021-04-07 22:08:17 +0200

    Update Reseau.hpp

--- troisième rendu/Reseau.hpp
+++ troisième rendu/Reseau.hpp
@@ -2,42 +2,37 @@
 #include <string>
 #include <list>
 
-#ifndef FICHIER_HPP
-#define FICHIER_HPP
+#ifndef RESEAU_HPP
+#define RESEAU_HPP
 
 using namespace std;
 
-class Reseau {
+// Classe abstraite JE CROIS PAS QUE CE SOIT UNE CLASSE ABSTRAITE EN FAIT OMG
+class Reseau { 
+ 
 
 	// Les attributs
-	private:
-		double solde;
-		static int nbC;
-		int numero;
-		list<string> titulaires;
+	protected: 		//pour que les classes filles aient également accès à ces attributs
+		static Couche Couches[];
+		static int nbC;	 //nombre de couches 
+		static double MatriceLiaisons[][];
+
 	
 	// Les constructeurs
 	public: 
-		Compte();
-		Compte(double s);
+		Reseau();
+		Reseau(const int nbC); //Constructeur pour le cas ou l'utilisateur décide du nombre de couches
 		
 	// Les methodes
 	public: 
-		void deposer(double const x);
-		void retirer(double const x);
-		double getSolde() const;	// Cette methode n'a pas le droit de modifier les attributs
-		static int nbComptes();
-		int getNumero() const;
-		void ajouterTitulaire(string nom);
-		bool retirerTitulaire(int i);
-		string operator [] (int i) const;
-		friend std::ostream& operator<< (std::ostream& flux, Compte const& compte);
-	private: 
-		void setSolde(double const x);
-		void afficher();
+		void ajouterCouche(Couche c, int numCouche);
+		double erreur(Fichier SortiesAttendues)
+		void ApprentissageSupervisé(Fichier Données);
+		void ApprentissageNonSupervisé(Fichier Données);
+		
 	
 };
 
-std::ostream& operator<< (std::ostream& flux, Compte const& compte);
+
 
 #endif
