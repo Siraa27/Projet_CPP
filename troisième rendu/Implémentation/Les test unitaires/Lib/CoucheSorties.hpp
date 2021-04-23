4bc9298011892ac7e6e019d5b4d74924d1940719
HELAL <mehdi.helal@insa-rouen.fr>
2021-04-23 20:53:59 +0200

    up

--- troisième rendu/Implémentation/Les test unitaires/Lib/CoucheSorties.hpp
+++ troisième rendu/Implémentation/Les test unitaires/Lib/CoucheSorties.hpp
@@ -41,6 +41,14 @@ class CoucheSorties: public Couche{
       *  \return matrice des liaisons
       */
       Matrice getLiaisonEntrees();
+      
+      /*!
+      *  \fn getBiais()
+      *  \brief Ce qui nous permet a acceder a la matrice biais de la couche
+      *  \return matrice des biais
+      */
+      Matrice getBiais();
+
       /*!
       *  \fn preActivation()
       *  \brief La fonction de pré activation : méthode qui permet de faire la somme pondérée des entrées
